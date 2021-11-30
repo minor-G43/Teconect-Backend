8bc5cc0b30a7912e45553582f37fa6b7047e496e
conqurer <56428363+somya339@users.noreply.github.com>
2021-11-30 14:47:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ server.listen(port, () => console.log(`Server is listening on port ${port}`));
 
 
 app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
